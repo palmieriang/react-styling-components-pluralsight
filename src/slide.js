52d484f26857d61cbe0ac2581b30c7a6478d351e
Angelo Palmieri <palmieri.ang@gmail.com>
2019-03-19 23:25:54 +0000

    first commit

---
+++ src/slide.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import styleable from 'react-styleable'
+
+import css from './slide.module.css'
+
+function Slide(props) {
+  return (
+    <article className={props.css.root} style={props.style}>
+      <img src={props.image} alt={props.title} />
+      <footer className={props.css.footer}>
+        <h2 className={props.css.title}>{props.title}</h2>
+        <div>{props.children}</div>
+      </footer>
+    </article>
+  )
+}
+
+Slide.propTypes = {
+  image: PropTypes.string.isRequired,
+  style: PropTypes.object,
+  title: PropTypes.string
+}
+
+export default styleable(css)(Slide)
